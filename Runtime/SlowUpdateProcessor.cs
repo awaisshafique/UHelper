ff6ffc8633be807692635e41a5b20db56260b528
awaisshafique <awais.shafique71@outlook.com>
2025-03-08 07:59:56 +0500

    Added namespace

--- Runtime/SlowUpdateProcessor.cs
+++ Runtime/SlowUpdateProcessor.cs
@@ -1,46 +1,50 @@
 using UnityEngine;
 using System;
 
-public class SlowUpdateProcessor
+namespace UHelper
 {
-    private readonly Action action;
-    private float interval;
-    private float nextActionTime;
-    private readonly bool useUnscaledTime;
 
-    public float Interval
+    public class SlowUpdateProcessor
     {
-        get => interval;
-        set
+        private readonly Action action;
+        private float interval;
+        private float nextActionTime;
+        private readonly bool useUnscaledTime;
+
+        public float Interval
         {
-            if (value > 0)
+            get => interval;
+            set
             {
-                float currentTime = GetCurrentTime();
-                float elapsedTime = currentTime - (nextActionTime - interval);
-                interval = value;
-                nextActionTime = currentTime + Mathf.Max(0, interval - elapsedTime);
+                if (value > 0)
+                {
+                    float currentTime = GetCurrentTime();
+                    float elapsedTime = currentTime - (nextActionTime - interval);
+                    interval = value;
+                    nextActionTime = currentTime + Mathf.Max(0, interval - elapsedTime);
+                }
             }
         }
+
+        public SlowUpdateProcessor(Action action, float interval, bool useUnscaledTime = false)
+        {
+            this.action = action ?? throw new ArgumentNullException(nameof(action));
+            this.interval = interval > 0 ? interval : throw new ArgumentOutOfRangeException(nameof(interval), "Interval must be greater than zero.");
+            this.useUnscaledTime = useUnscaledTime;
+
+            nextActionTime = GetCurrentTime() + this.interval;
+        }
+
+        public void Update()
+        {
+            float currentTime = GetCurrentTime();
+
+            if (!(currentTime >= nextActionTime)) return;
+
+            action.Invoke();
+            nextActionTime = currentTime + interval;
+        }
+
+        private float GetCurrentTime() => useUnscaledTime ? Time.unscaledTime : Time.time;
     }
-
-    public SlowUpdateProcessor(Action action, float interval, bool useUnscaledTime = false)
-    {
-        this.action = action ?? throw new ArgumentNullException(nameof(action));
-        this.interval = interval > 0 ? interval : throw new ArgumentOutOfRangeException(nameof(interval), "Interval must be greater than zero.");
-        this.useUnscaledTime = useUnscaledTime;
-
-        nextActionTime = GetCurrentTime() + this.interval;
-    }
-
-    public void Update()
-    {
-        float currentTime = GetCurrentTime();
-
-        if (!(currentTime >= nextActionTime)) return;
-
-        action.Invoke();
-        nextActionTime = currentTime + interval;
-    }
-
-    private float GetCurrentTime() => useUnscaledTime ? Time.unscaledTime : Time.time;
 }
