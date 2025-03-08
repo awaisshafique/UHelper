4a736e656e747451634e0cadc6bb2796082d6518
awaisshafique <awais.shafique71@outlook.com>
2025-03-08 07:23:15 +0500

    Initial commit - Added UHelper package structure

---
+++ Runtime/SlowUpdateProcessor.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System;
+
+public class SlowUpdateProcessor
+{
+    private readonly Action action;
+    private float interval;
+    private float nextActionTime;
+    private readonly bool useUnscaledTime;
+
+    public float Interval
+    {
+        get => interval;
+        set
+        {
+            if (value > 0)
+            {
+                float currentTime = GetCurrentTime();
+                float elapsedTime = currentTime - (nextActionTime - interval);
+                interval = value;
+                nextActionTime = currentTime + Mathf.Max(0, interval - elapsedTime);
+            }
+        }
+    }
+
+    public SlowUpdateProcessor(Action action, float interval, bool useUnscaledTime = false)
+    {
+        this.action = action ?? throw new ArgumentNullException(nameof(action));
+        this.interval = interval > 0 ? interval : throw new ArgumentOutOfRangeException(nameof(interval), "Interval must be greater than zero.");
+        this.useUnscaledTime = useUnscaledTime;
+
+        nextActionTime = GetCurrentTime() + this.interval;
+    }
+
+    public void Update()
+    {
+        float currentTime = GetCurrentTime();
+
+        if (!(currentTime >= nextActionTime)) return;
+
+        action.Invoke();
+        nextActionTime = currentTime + interval;
+    }
+
+    private float GetCurrentTime() => useUnscaledTime ? Time.unscaledTime : Time.time;
+}
